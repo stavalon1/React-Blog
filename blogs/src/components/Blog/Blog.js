c087bfc00d31d4719c51c11352317bb06c163034
stav <stavalon31@gmail.com>
2021-07-19 19:58:14 +0300

    version_2

---
+++ blogs/src/components/Blog/Blog.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './blog.css'
+
+export default function Blog() {
+    return(
+        <div className='blog-container'>
+            <div className='left-image'></div>
+
+            <div className='details'>
+           <p>Adopt a Dog</p>
+           <p>Save a life and make a friend.</p>
+        </div>
+        </div>
+       
+    )
+}
